5ad62e06e7519331fd2342cd100c6ba05f1b9b41
soonuse <116975428@qq.com>
2019-12-12 21:52:54 +0800

    Remove test code

--- ina219.py
+++ ina219.py
@@ -153,16 +153,3 @@ class INA219:
     def get_shunt_voltage_mV(self):
         raw = self.read_word(INA219_REG_SHUNT_VOLTAGE)
         return raw * 0.01
-
-
-if __name__ == "__main__":
-    ina219 = INA219(address=INA219_ADDRESS)
-    try:
-        print('bus voltage %d mV' % ina219.get_bus_voltage_mV())
-    except AssertionError as e:
-        print(repr(e))
-        print('Retry...')
-        print('bus voltage %d mV' % ina219.get_bus_voltage_mV())
-    print('shunt voltage %.3f mV' % ina219.get_shunt_voltage_mV())
-    print('current %.3f mA' % ina219.get_shunt_voltage_mV())
-    print('power %.3f mW' % ina219.get_shunt_voltage_mV())
